5fa9f72519942e7c12a035b94f49bd205557e818
Mauricio Nunes <mutila@gmail.com>
2025-07-12 10:07:51 -0300

    nova rota

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -3,6 +3,7 @@ return [
     'routes' => [
         ['name' => 'page#index', 'url' => '/', 'verb' => 'GET'],
         ['name' => 'search#search', 'url' => '/api/search', 'verb' => 'POST'],
+        ['name' => 'search#searchWithFilters', 'url' => '/api/search/filters', 'verb' => 'GET'],
         ['name' => 'search#getTags', 'url' => '/api/tags', 'verb' => 'GET'],
     ]
 ];
